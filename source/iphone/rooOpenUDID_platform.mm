3e4139c5a2abb36747fdac07915661ecd65dbc27
My Name <user@user-desktop>
2012-07-27 14:19:13 +0300

    fix - upload correct version

--- source/iphone/rooOpenUDID_platform.mm
+++ source/iphone/rooOpenUDID_platform.mm
@@ -8,39 +8,48 @@
  */
 #include "rooOpenUDID_internal.h"
 #include "OpenUDID.h"
+#include "s3eDebug.h"
 
-OpenUDID * g_rooOpenUDID; 
 const char * g_rooOpenUDID_str; 
 s3eResult rooOpenUDIDInit_platform()
 {
-    // Add any platform-specific initialisation code here
-    g_rooOpenUDID = [[OpenUDID alloc] init];
+    s3eDebugOutputString("rooOpenUDIDInit_platform");
     g_rooOpenUDID_str = nil;
     return S3E_RESULT_SUCCESS;
 }
 
 void rooOpenUDIDTerminate_platform()
 {
-    [g_rooOpenUDID release];
+    s3eDebugOutputString("rooOpenUDIDTerminate_platform");
+
     if(g_rooOpenUDID_str)
         free((void*)g_rooOpenUDID_str);
-    // Add any platform-specific termination code here
 }
 
 void rooOpenUDID_init_platform()
 {
+    s3eDebugOutputString("rooOpenUDID_init_platform");
 }
 
 bool rooOpenUDID_isInitialized_platform()
 {
+    s3eDebugOutputString("rooOpenUDID_isInitialized_platform");
     return true;
 }
 
 const char * rooOpenUDID_getValue_platform()
 {
-    NSString* s1 = [g_rooOpenUDID value];
-    if(g_rooOpenUDID_str)
-        free((void*)g_rooOpenUDID_str);
-    g_rooOpenUDID_str = strdup([s1 UTF8String]);
+    s3eDebugOutputString("rooOpenUDID_getValue_platform");
+
+    {
+        NSString* s1 = [OpenUDID value];
+        s3eDebugOutputString("5");
+        if(g_rooOpenUDID_str)
+            free((void*)g_rooOpenUDID_str);
+        g_rooOpenUDID_str = strdup([s1 UTF8String]);
+        s3eDebugOutputString(g_rooOpenUDID_str);
+        s3eDebugOutputString("5");
+        
+    }
     return g_rooOpenUDID_str;
 }
